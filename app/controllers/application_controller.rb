db79d4b5ffd41f9251c31183fd598380c47e93a9
khun84 <khun84@gmail.com>
2021-10-02 10:46:15 +0800

    Setup devise auth and registrations controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_if_not_authorize
+
+  private
+
+  def redirect_if_not_authorize
+    unless user_signed_in?
+      redirect_to login_path
+    end
+  end
 end
+
